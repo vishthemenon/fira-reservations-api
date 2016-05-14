475e1fc4b90d7f70ee92141c0fc8de7bb61740e1
Vishnu R Menon <vishthemenon@gmail.com>
2016-05-14 16:40:45 +0800

    refactored get/reservations

--- server.js
+++ server.js
@@ -2,60 +2,53 @@ r = require('rethinkdb')
 var host = 'ec2-52-74-122-161.ap-southeast-1.compute.amazonaws.com'
 var port = 28015
 var app = require('express')()
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
+var validator = require('express-validator')
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+app.use(validator())
 
 
 var connection = null
-r .connect({
+
+r.connect({
   host,
   port,
   user: "admin",
   password: "angelhack",
-  db : "fira"
-}, function(err, conn) {
-  if(err) throw err;
-  connection = conn;
-  test()
-});
-
-
-function test() {
-  r.table("reservations").insert({
-    "title": "Lorem ipsum",
-    "content": "Dolor sit amet"
-  }).run(connection)
-}
-
-
+  db : "fira"},
+  function(err, conn) {
+    if(err) throw err;
+    connection = conn;
+    console.log("Connected to rethinkdb")
+  }
+)
 
 app.get('/reservations', function(req, res){
+  const e = []
   r.table('reservations').run(connection, function(err, cursor) {
     cursor.toArray(function(err, result) {
       if(err) {
         return next(err);
       }
-      console.log(result)
       res.send(result)
-    });
+      // result.map(i => console.log(i.name))
+    })
   })
 })
 
 app.post('/new', function(req, res){
   r.table("reservations").insert({name:req.body.name,
-  phone:req.body.phone}).run(connection, function(err){
-    if (err) {
-      console.log(err)
-      res.send(err)
-    }
-    res.sendStatus(200)
+    phone:req.body.phone}).run(connection, function(err){
+      if (err) {
+        console.log(err)
+        res.send(err)
+      }
+      res.sendStatus(200)
+    })
   })
-
-
-})
 
 app.listen(3000, function(err){
   if (err) throw err
-  console.log("Cool yeah")
+  console.log("Reservation server listening on port 3000")
 })
